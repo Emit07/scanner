14c5436ab6fade078eef0eb3c253a71b8a5d8f49
Ale De Leo <adv.deleo@gmail.com>
2021-04-15 18:31:15 -0700

    fixed threads

--- scanner.py
+++ scanner.py
@@ -4,24 +4,68 @@ import threading
 from queue import Queue
 import sys
 import ipaddress
+import socket
 import time
 import os
 
 class Scanner(object):
 
-	def scan(self):
-		net4 = ipaddress.ip_network("10.0.0.0/24")
-		for x in net4.hosts():
-			try:
-				HOST_UP  = True if os.system("ping -c 1 " + str(x) + "2>&1 >/dev/null") == 0 else False
-				print(HOST_UP)
-			except KeyboardInterrupt:
-				sys.exit()
+    def worker(self):
+        while True:
+            item = self._queue.get()
+            port, attempt = self.scan(item)
 
-	def __init__(self, ip : str):
-		self._ip = ip
+            self._ports.append((port, attempt))
 
-		self.scan()
+            self._queue.task_done()
+
+    def scan(self, port): # -> tuple:
+        attempt = self._socket.connect_ex((self._host, port))
+
+        return (port, attempt)
+
+    def run(self):
+
+    	self._queue = Queue()
+    	self._ports = []
+
+    	for i in range(self.threads):
+    		t = threading.Thread(target=self.worker, daemon=True)
+    		t.start()
+
+    	for worker in range(1, 10000):
+    		self._queue.put(worker)
+
+    	self._queue.join()
+
+    def display(self):
+
+        self._ports.sort()
+
+        for port in self._ports:
+            # if port[1] == True:
+            print(port[0], port[1])
+
+    def repr(self):
+
+        return "<{}>".format(self._range)
+
+    def __init__(self, host, threads=100):
+
+        self._start = time.time()
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._host = host
+
+        self._lock = threading.Lock()
+        self.threads = 100
+
+        socket.setdefaulttimeout(1)
+
+        self.run()
+        self.display()
+
+        with self._lock: print(time.time() - self._start)
 
 def main() -> int:
 
